5f04f5711c34c9889f9657c0873ed2166ad66399
MissingNo <514335188@qq.com>
2016-03-19 19:06:08 +0800

    解决画面失真。。

--- app/src/main/java/pers/missingno/protractor/activity/MainActivity.java
+++ app/src/main/java/pers/missingno/protractor/activity/MainActivity.java
@@ -2,6 +2,7 @@ package pers.missingno.protractor.activity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
@@ -15,6 +16,8 @@ import android.os.Handler;
 import android.os.HandlerThread;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.util.Size;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -23,16 +26,21 @@ import android.view.Surface;
 import android.view.TextureView;
 import android.view.WindowManager;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import pers.missingno.protractor.R;
+import pers.missingno.protractor.view.AutoFitTextureView;
 import pers.missingno.protractor.view.ProtractorView;
 
 public class MainActivity extends AppCompatActivity {
 
     private CameraDevice device;
     private CameraCaptureSession session;
-    private TextureView preview;
+    private AutoFitTextureView preview;
     private ProtractorView ruler;
 
     @Override
@@ -45,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         ruler= (ProtractorView) findViewById(R.id.ruler);
         ruler.setZOrderOnTop(true);
 
-        preview = (TextureView) findViewById(R.id.preview);
+        preview = (AutoFitTextureView) findViewById(R.id.preview);
 
         initCamera();
     }
@@ -84,6 +92,69 @@ public class MainActivity extends AppCompatActivity {
         return super.onKeyDown(keyCode, event);
     }
 
+    /**
+     * Compares two {@code Size}s based on their areas.
+     */
+    static class CompareSizesByArea implements Comparator<Size> {
+
+        @Override
+        public int compare(Size lhs, Size rhs) {
+            // We cast here to ensure the multiplications won't overflow
+            return Long.signum((long) lhs.getWidth() * lhs.getHeight() -
+                    (long) rhs.getWidth() * rhs.getHeight());
+        }
+
+    }
+
+    /**
+     * Given {@code choices} of {@code Size}s supported by a camera, choose the smallest one that
+     * is at least as large as the respective texture view size, and that is at most as large as the
+     * respective max size, and whose aspect ratio matches with the specified value. If such size
+     * doesn't exist, choose the largest one that is at most as large as the respective max size,
+     * and whose aspect ratio matches with the specified value.
+     *
+     * @param choices           The list of sizes that the camera supports for the intended output
+     *                          class
+     * @param textureViewWidth  The width of the texture view relative to sensor coordinate
+     * @param textureViewHeight The height of the texture view relative to sensor coordinate
+     * @param maxWidth          The maximum width that can be chosen
+     * @param maxHeight         The maximum height that can be chosen
+     * @param aspectRatio       The aspect ratio
+     * @return The optimal {@code Size}, or an arbitrary one if none were big enough
+     */
+    private static Size chooseOptimalSize(Size[] choices, int textureViewWidth,
+                                          int textureViewHeight, int maxWidth, int maxHeight, Size aspectRatio) {
+
+        // Collect the supported resolutions that are at least as big as the preview Surface
+        List<Size> bigEnough = new ArrayList<>();
+        // Collect the supported resolutions that are smaller than the preview Surface
+        List<Size> notBigEnough = new ArrayList<>();
+        int w = aspectRatio.getWidth();
+        int h = aspectRatio.getHeight();
+        for (Size option : choices) {
+            if (option.getWidth() <= maxWidth && option.getHeight() <= maxHeight &&
+                    option.getHeight() == option.getWidth() * h / w) {
+                if (option.getWidth() >= textureViewWidth &&
+                        option.getHeight() >= textureViewHeight) {
+                    bigEnough.add(option);
+                } else {
+                    notBigEnough.add(option);
+                }
+            }
+        }
+
+        // Pick the smallest of those big enough. If there is no one big enough, pick the
+        // largest of those not big enough.
+        if (bigEnough.size() > 0) {
+            return Collections.min(bigEnough, new CompareSizesByArea());
+        } else if (notBigEnough.size() > 0) {
+            return Collections.max(notBigEnough, new CompareSizesByArea());
+        } else {
+            Log.e("TAG", "Couldn't find any suitable preview size");
+            return choices[0];
+        }
+    }
+
     private void initCamera(){
         final CameraManager cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
         CameraCharacteristics characteristics = null;
@@ -93,7 +164,19 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
-        final Size previewSize=map.getOutputSizes(SurfaceTexture.class)[0];
+
+
+        DisplayMetrics metrics=new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        final Size previewSize=chooseOptimalSize(map.getOutputSizes(SurfaceTexture.class),
+                preview.getWidth(), preview.getHeight(), metrics.widthPixels,
+                metrics.heightPixels, Collections.max(
+                        Arrays.asList(map.getOutputSizes(ImageFormat.JPEG)),
+                        new CompareSizesByArea()));
+
+        preview.setAspectRatio(previewSize.getHeight(),previewSize.getWidth());
+
+
 
         preview.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
             @Override
